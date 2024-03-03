3e64d2f3ff60312ccf8c87014de2a63399845155
Sergey <serejatoje@gmail.com>
2024-03-04 02:10:08 +0700

    modals.html

--- main.js
+++ main.js
@@ -1,54 +1,58 @@
 import './sass/account.sass'
 
 $(function () {
-    $('select').niceSelect();
 
-    const counter = (el) => {
-        const duration = document.querySelector('[data-duration]').getAttribute('data-duration')
-        const start = parseInt(el.textContent, 10)
-        const end = parseInt(el.dataset.counter, 10)
-        if (start === end) return
-        const range = end - start
-        let curr = start
-        const timeStart = Date.now()
-        const loop = () => {
-            let elaps = Date.now() - timeStart
-            if (elaps > duration) elaps = duration
-            const frac = elaps / duration
-            const step = frac * range
-            curr = start + step
-            el.textContent = Math.trunc(curr)
-            if (elaps < duration) requestAnimationFrame(loop)
-        };
-        requestAnimationFrame(loop)
+    if ($('select').length > 0) {
+        $('select').niceSelect();
     }
 
-    document.querySelectorAll("[data-counter]").forEach(counter)
+    if ($('[data-duration]').length > 0) {
 
-    Fancybox.bind(document.querySelector("[data-fancybox]"), {
+        const counter = (el) => {
+            const duration = document.querySelector('[data-duration]').getAttribute('data-duration')
+            const start = parseInt(el.textContent, 10)
+            const end = parseInt(el.dataset.counter, 10)
+            if (start === end) return
+            const range = end - start
+            let curr = start
+            const timeStart = Date.now()
+            const loop = () => {
+                let elaps = Date.now() - timeStart
+                if (elaps > duration) elaps = duration
+                const frac = elaps / duration
+                const step = frac * range
+                curr = start + step
+                el.textContent = Math.trunc(curr)
+                if (elaps < duration) requestAnimationFrame(loop)
+            };
+            requestAnimationFrame(loop)
+        }
 
-    });
+        document.querySelectorAll("[data-counter]").forEach(counter)
 
-    // const progress = (el) => {
-    //     const duration = 500
-    //     const start = parseInt(el.dataset.start, 10)
-    //     const end = parseInt(el.dataset.finish, 10)
-    //     if (start === end) return
-    //     const range = end - start
-    //     let curr = start
-    //     const timeStart = Date.now()
-    //     const loop = () => {
-    //         let elaps = Date.now() - timeStart
-    //         if (elaps > duration) elaps = duration
-    //         const frac = elaps / duration
-    //         const step = frac * range
-    //         curr = start + step
-    //         el.setAttribute('data-number', Math.trunc(curr) + "%");
-    //         el.style.setProperty('--progress', Math.trunc(curr) + "%");
-    //         if (elaps < duration) requestAnimationFrame(loop)
-    //     };
-    //     requestAnimationFrame(loop)
-    // }
-    // document.querySelectorAll(".welcome-progress").forEach(progress)
+        Fancybox.bind(document.querySelector("[data-fancybox]"), {});
 
+        // const progress = (el) => {
+        //     const duration = 500
+        //     const start = parseInt(el.dataset.start, 10)
+        //     const end = parseInt(el.dataset.finish, 10)
+        //     if (start === end) return
+        //     const range = end - start
+        //     let curr = start
+        //     const timeStart = Date.now()
+        //     const loop = () => {
+        //         let elaps = Date.now() - timeStart
+        //         if (elaps > duration) elaps = duration
+        //         const frac = elaps / duration
+        //         const step = frac * range
+        //         curr = start + step
+        //         el.setAttribute('data-number', Math.trunc(curr) + "%");
+        //         el.style.setProperty('--progress', Math.trunc(curr) + "%");
+        //         if (elaps < duration) requestAnimationFrame(loop)
+        //     };
+        //     requestAnimationFrame(loop)
+        // }
+        // document.querySelectorAll(".welcome-progress").forEach(progress)
+
+    }
 })
